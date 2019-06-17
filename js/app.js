9308b9c4e1a0ea92ee47f8615e203928dbec4848
ginapult <gina.pultorak@gmail.com>
2019-06-17 15:57:23 -0700

    added js skeleton

---
+++ js/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+//construtor function to create an object for each image
+//properties: name, filePath, description, number of times that has been shown, number of times that hs been clicked ,array of each instance
+
+//function to get random numbers
+
+//function to deduplicate
+
+//function to render images + descriptions
+
+//addEventListener for click
+
+//clickEventListener(user initial count)
+
+//function to remove eventlistener (after 25 clicks)
+
+//function to calculate percentage of clicks 
+
